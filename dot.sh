6e893f8faf3c98853ee47861fe15be4464f482dc
jpacs <1902737+jpacs@users.noreply.github.com>
2022-05-13 23:27:16 +0100

    added picom.conf

--- dot.sh
+++ dot.sh
@@ -1,16 +1,21 @@
-if [[ $1  ]]; then
-    echo "in here"
-    case $1 in
+if [[ "$1"  ]]; then
+    case "$1" in
         -down )
-        echo "files being sourced"
             cp ~/.bashrc .bashrc
             cp ~/.config/bspwm/bspwmrc bspwmrc
             cp ~/.config/sxhkd/sxhkdrc sxhkdrc
             cp ~/.config/kitty/kitty.conf kitty.conf
             cp ~/.config/nvim/init.vim init.vim
-        echo "all done!"
+            cp ~/.config/picom/picom.conf picom.conf
         ;;
         -up )
+            cp .bashrc ~/.bashrc 
+            cp bspwmrc ~/.config/bspwm/bspwmrc
+            cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+            cp kitty.conf ~/.config/kitty/kitty.conf
+            cp init.vim ~/.config/nvim/init.vim
+            cp picom.conf ~/.config/picom/picom.conf
+
         ;;
     esac
 fi
